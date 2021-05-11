94aac1e58389b1eb942ae7914e0abbd6a6f12703
Ravshan <jumanazarovravshan07@gmail.com>
2021-05-11 13:07:27 +0500

    optimallashtirildi
    bisotimizdagi harflar qo'shildi

--- find_word/back.py
+++ find_word/back.py
@@ -1,5 +1,9 @@
 from uzwords import words
 import random
+xarf=['й','ц','у','к','е','н','г','ш','ў',
+   'з','х','ъ','ҳ','э','ж','д','л','о',
+   'р','п','а','в','қ','ф','я','ч','с',
+   'м','и','т','ь','б','ю']
 def letter(word):
     l=len(word)
     found=[]
@@ -9,10 +13,10 @@ def letter(word):
         found.append('_')
     while True:
         k+=1
-        a = input("\nharf kiriting:")
+        a = input("\nharf kiriting:").lower()
         if a not in entered_words:
             entered_words+=a
-
+            xarf.remove(a)
             if a in word:
                 for i in range(len(word)):
                     if a == word[i]:
@@ -33,7 +37,8 @@ def letter(word):
         else:
             print(entered_words)
             print("Bu harf avval kiritilgan.Iltimos boshqa xarf kiriting")
-        print(f"\nShu vaqtgacha kiritilgan harflar {entered_words.upper()}")
+        print(f"\nShu vaqtgacha kiritilgan harflar {entered_words.upper()}"
+              f"\nBisotingizdagi harflar {xarf}")
         if l == 0:
             break
     jovob=f"Siz {k} ta urinishda topdingiz"
